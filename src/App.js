6b848a1e337b1b1b802549fb968de4458347c930
NisthaAB <nistha.ghorela@almabetter.com>
2023-05-18 16:51:19 +0530

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Navbar from './components/header/header.jsx'
 import Home from "./components/Home/home"
+import Genre from './components/Genre/Genre';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Home/>
+    <Navbar />
+    <Router>
+      <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/genre" element={<Genre />}></Route>
+        </Routes>
+    </Router>
     </>
   );
 }
