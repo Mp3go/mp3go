47212ac4fb4ecf4afa5bec6d642607bcbae7fdc6
Gaurav verma <gaurav.verma@almabetter.com>
2023-05-18 18:35:26 +0530

    added toggle

---
+++ src/shared/const.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+const Theme = {
+    Light: 'light',
+    Dark: 'dark',
+    CurrentTheme: 'current-theme'
+  };
+  
+  export default Theme;
