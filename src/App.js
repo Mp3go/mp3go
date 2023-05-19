3a2c7ce65574951484f20a6fedf6e4e1063a3575
NisthaAB <nistha.ghorela@almabetter.com>
2023-05-19 15:42:02 +0530

    Modified Genre component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import './App.css';
-import Navbar from './components/header/header.jsx'
+import Navbar from './components/header/header.jsx';
 import Allalbums from './components/AlbumsPage/albums';
-import Home from "./components/Home/home"
+import Home from './components/Home/home';
 import Genre from './components/Genre/Genre';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Router>
-      <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route path="/albums" element={<Allalbums/>}></Route>
-          <Route path="/genre/:genreType" element={<Genre />}></Route>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />}></Route>
+          <Route path='/albums' element={<Allalbums />}></Route>
+          <Route path='/genre/:genreType' element={<Genre />}></Route>
         </Routes>
-    </Router>
+      </Router>
     </>
   );
 }
