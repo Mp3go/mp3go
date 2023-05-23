125841c0a9502766529a24290620f831845310bc
NisthaAB <nistha.ghorela@almabetter.com>
2023-05-23 14:19:00 +0530

    Modified Card component

--- src/components/Home/Main/FeaturedAlbums.js
+++ src/components/Home/Main/FeaturedAlbums.js
@@ -1,50 +1,32 @@
-import React, { useState } from "react";
-import Card from "./card";
-import { Ripple, initTE } from "tw-elements";
+import React, { useState } from 'react';
+import Card from './card';
+import { Ripple, initTE } from 'tw-elements';
+import data from '../../data';
+import AlbumList from '../../AlbumList';
 
 initTE({ Ripple });
 
 export default function FeaturedAlbums() {
-  const [featuredAlbums, setfeaturedAlbums] = useState([
-    {
-      image: "https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg",
-      title: "Card title",
-      description:
-        "Some quick example text to build on the card title and make up the bulk of the card's content",
-    },
-    {
-      image: "https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg",
-      title: "Card title",
-      description:
-        "Some quick example text to build on the card title and make up the bulk of the card's content",
-    },
-    {
-      image: "https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg",
-      title: "Card title",
-      description:
-        "Some quick example text to build on the card title and make up the bulk of the card's content",
-    },
-    {
-      image: "https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg",
-      title: "Card title",
-      description:
-        "Some quick example text to build on the card title and make up the bulk of the card's content",
-    },
-    {
-      image: "https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg",
-      title: "Card title",
-      description:
-        "Some quick example text to build on the card title and make up the bulk of the card's content",
-    },
-  ]);
+  const array = data;
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  const randomElements = shuffleArray(array).slice(0, 5);
+
+  const [featuredAlbums, setfeaturedAlbums] = useState(randomElements);
 
   return (
-    <div className="mx-4 my-4 py-2">
-      <h1 className="mt-10 mb-2 text-center font-sans capitalize text-5xl font-medium">
+    <div className='mx-4 my-4 py-2'>
+      <h1 className='mt-10 mb-2 text-center font-sans capitalize text-5xl font-medium'>
         Featured Albums
       </h1>
-      <hr className="w-1/3 mx-auto" />
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-10  justify-center mt-5">
+      <hr className='w-1/3 mx-auto' />
+      {/* <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-10  justify-center mt-5">
         {featuredAlbums.map((card) => (
           <Card
             image={card.image}
@@ -52,9 +34,12 @@ export default function FeaturedAlbums() {
             description={card.description}
           />
         ))}
-      </div>
-      <div className="mt-0 flex justify-center ">
-        <a href="/albums" className="text-center text-base hover:text-xl">
+      </div> */}
+
+      <AlbumList albums={featuredAlbums} />
+
+      <div className='mt-2 flex justify-center text-gray-500 hover:text-black cursor-pointer'>
+        <a href='/albums' className='text-center'>
           see more
         </a>
       </div>
