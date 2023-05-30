9b284a34cf5236125fe607f7075a40cab3887ae3
NisthaAB <nistha.ghorela@almabetter.com>
2023-05-30 17:51:18 +0530

    Added Routes

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const authRoutes = require('./routes/auth')
+const albumRoutes = require('./routes/albums');
+const userRoutes = require('./controllers/user')
 
 const app = express();
+
+app.use('/albums', albumRoutes);
+app.use(authRoutes);
+app.use('/user', userRoutes);
 
 app.listen(3000, () => {
   console.log("Listening on Port");
--- src/components/Home/Main/NewReleases.js
+++ src/components/Home/Main/NewReleases.js
@@ -4,11 +4,15 @@ import data from "../../data";
 
 import { Ripple, initTE } from "tw-elements";
 import AlbumList from "../../AlbumList";
+import { async } from "q";
+import { catchClause } from "@babel/types";
+import axios from "axios";
+import { useAxios } from "../../../hooks/useAxios";
 
 initTE({ Ripple });
 
 export default function NewReleases() {
-  const [newReleases, setNewReleases] = useState(data);
+  const {data, error} = useAxios('/albums/new-releases', "GET");
 
   return (
     <div className="mx-4 my-4 py-2 px-5">
@@ -18,7 +22,7 @@ export default function NewReleases() {
       <hr className="w-1/5 mx-auto " />
       {/* <AlbumList albums={newReleases} /> */}
       <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-10 justify-center mt-5'>
-        {newReleases.map((album) => (
+        {data.map((album) => (
           <Card
             id = {album.id}
             image = {album.imagepath}
