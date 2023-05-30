4bc92947258e94fbdd4ec33445e9b6b532c4b9b2
rachit-almabetter <133861502+rachit-almabetter@users.noreply.github.com>
2023-05-30 17:37:00 +0530

    Backend Changes
    Added Backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const axios = require("axios");
+const Music = require("./models/music");
+const Filter = require("./models/filter");
+require("dotenv").config();
+const db = require("./config/dbconfig");
 
 const app = express();
 
