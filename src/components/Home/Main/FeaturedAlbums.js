54402c52ff216b3c33f093da3234eb6725f9e5bb
NisthaAB <nistha.ghorela@almabetter.com>
2023-06-01 17:31:07 +0530

    Added useAxios hook, language dropdown and language page functioning

--- src/components/Home/Main/FeaturedAlbums.js
+++ src/components/Home/Main/FeaturedAlbums.js
@@ -1,46 +1,38 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { Ripple, initTE } from "tw-elements";
-import data from "../../data";
-import AlbumList from "../../AlbumList";
-import Card from "./card"; 
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Ripple, initTE } from 'tw-elements';
+import data from '../../data';
+import AlbumList from '../../AlbumList';
+import Card from './card';
+import { useAxios } from '../../../hooks/useAxios';
 
 initTE({ Ripple });
 
 export default function FeaturedAlbums() {
-  const array = data;
-  const shuffleArray = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  };
-
-  const randomElements = shuffleArray(array).slice(0, 5);
-
-  const {data, error} = useAxios('/albums/featured-albums', "GET");
+  const { data, error } = useAxios('/albums/featured-albums', 'GET');
 
   return (
-    <div className="mx-4 mt-4 py-2 px-5">
-      <h1 className="mt-10 mb-2 text-center font-sans capitalize text-5xl font-medium">
+    <div className='mx-4 mt-4 py-2 px-5'>
+      <h1 className='mt-10 mb-2 text-center font-sans capitalize text-5xl font-medium'>
         Featured Albums
       </h1>
-      <hr className="w-[25%] mx-auto" />
+      <hr className='w-[25%] mx-auto' />
       {/* <AlbumList albums={featuredAlbums} /> */}
       <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-10 justify-center mt-5'>
-        {data.map((album) => (
-          <Card
-            id = {album.id}
-            image = {album.imagepath}
-            price = {album.price}
-            title={album.albumName}
-          />
-        ))}
-        </div>
-      <div className="mt-[5rem] flex justify-center ">
-        <Link to="/albums">
-          <button className="text-center text-white rounded-md px-[2rem] py-4 hover:text-gray cursor-pointer bg-black">
+        {data
+          ? data.map((album) => (
+              <Card
+                id={album.id}
+                image={album.img}
+                price={album.price}
+                title={album.name}
+              />
+            ))
+          : null}
+      </div>
+      <div className='mt-[5rem] flex justify-center '>
+        <Link to='/albums'>
+          <button className='text-center text-white rounded-md px-[2rem] py-4 hover:text-gray cursor-pointer bg-black'>
             See More
           </button>
         </Link>
--- src/components/Home/Main/NewReleases.js
+++ src/components/Home/Main/NewReleases.js
@@ -1,35 +1,42 @@
-import React, { useState } from "react";
-import Card from "./card";
-import data from "../../data";
-
-import { Ripple, initTE } from "tw-elements";
-import AlbumList from "../../AlbumList";
-import { async } from "q";
-import { catchClause } from "@babel/types";
-import axios from "axios";
-import { useAxios } from "../../../hooks/useAxios";
+import React, { useState } from 'react';
+import Card from './card';
+import data from '../../data';
+import axiosAPI from '../../../axios';
+import { Ripple, initTE } from 'tw-elements';
+import AlbumList from '../../AlbumList';
+import { async } from 'q';
+import { catchClause } from '@babel/types';
+import { useAxios } from '../../../hooks/useAxios';
 
 initTE({ Ripple });
 
 export default function NewReleases() {
-  const {data, error} = useAxios('/albums/new-releases', "GET");
+  // const {data, error} = useAxios('/albums/all', "GET");
+  // console.log(data);
+  // const getData = async () => {}
+  // const data = await axiosAPI.get("/albums/all");
+  const { data, error } = useAxios('/albums/new-releases', 'GET');
+  // console.log('In new releases', data);
 
+  // getData();
   return (
-    <div className="mx-4 my-4 py-2 px-5">
-      <h1 className="mt-10 mb-2 text-center font-sans capitalize text-5xl font-medium">
+    <div className='mx-4 my-4 py-2 px-5'>
+      <h1 className='mt-10 mb-2 text-center font-sans capitalize text-5xl font-medium'>
         New Releases
       </h1>
-      <hr className="w-1/5 mx-auto " />
+      <hr className='w-1/5 mx-auto ' />
       {/* <AlbumList albums={newReleases} /> */}
       <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-10 justify-center mt-5'>
-        {data.map((album) => (
-          <Card
-            id = {album.id}
-            image = {album.imagepath}
-            price = {album.price}
-            title={album.albumName}
-          />
-        ))}
+        {data
+          ? data.map((album) => (
+              <Card
+                id={album.id}
+                image={album.img}
+                price={album.price}
+                title={album.name}
+              />
+            ))
+          : null}
       </div>
     </div>
   );
